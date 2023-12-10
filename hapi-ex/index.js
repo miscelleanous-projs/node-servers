2eb68da15b4236800f80ef7fce77623959ebb42a
Menjanahary <menjaraz@gmail.com>
2023-12-10 23:50:27 +0400

    Update index.js

--- hapi-ex/index.js
+++ hapi-ex/index.js
@@ -1,31 +1,26 @@
-'use strict';
-
-const Hapi = require('@hapi/hapi');
+import Hapi from '@hapi/hapi';
 
 const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+  });
 
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
-    });
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return 'Hello Hapi!';
+    },
+  });
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-
-            return 'Hello Hapi!';
-        }
-    });
-
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
 };
 
 process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
+  console.log(err);
+  process.exit(1);
 });
 
 init();
